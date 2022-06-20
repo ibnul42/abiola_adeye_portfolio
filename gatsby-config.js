180e501e3f3caab1b07d2076f34a5cb5dd363d9b
Mohiuddin Rabby <rabbymohiuddin@gmail.com>
2022-06-21 00:33:58 +0600

    description & ico icon changed

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,8 +20,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'BrittanyChiang',
-        short_name: 'BrittanyChiang',
+        name: 'ABIOLA',
+        short_name: 'ABIOLA',
         start_url: '/',
         background_color: config.colors.darkNavy,
         theme_color: config.colors.navy,
