3885ee34c58f4131d1561c8b11ce981acc5ffacb
Ibnul Ashir <ibnulashir42@gmail.com>
2022-06-24 15:21:50 +0600

    changed

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from 'react';
-import PropTypes from 'prop-types';
+import React, { useEffect } from 'react';
+// import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import { Icon } from '@components/icons';
 import { socialMedia } from '@config';
@@ -62,25 +62,25 @@ const StyledCredit = styled.div`
 `;
 
 const Footer = () => {
-  const [githubInfo, setGitHubInfo] = useState({
-    stars: null,
-    forks: null,
-  });
+  // const [githubInfo, setGitHubInfo] = useState({
+  //   stars: null,
+  //   forks: null,
+  // });
 
   useEffect(() => {
     if (process.env.NODE_ENV !== 'production') {
       return;
     }
-    fetch('https://api.github.com/repos/bchiang7/v4')
-      .then(response => response.json())
-      .then(json => {
-        const { stargazers_count, forks_count } = json;
-        setGitHubInfo({
-          stars: stargazers_count,
-          forks: forks_count,
-        });
-      })
-      .catch(e => console.error(e));
+    // fetch('https://api.github.com/repos/bchiang7/v4')
+    //   .then(response => response.json())
+    //   .then(json => {
+    //     // const { stargazers_count, forks_count } = json;
+    //     // setGitHubInfo({
+    //     //   stars: stargazers_count,
+    //     //   forks: forks_count,
+    //     // });
+    //   })
+    //   .catch(e => console.error(e));
   }, []);
 
   return (
@@ -102,7 +102,7 @@ const Footer = () => {
         <a href="https://github.com/bchiang7/v4">
           <div>Theme credit to Brittany Chiang</div>
 
-          {githubInfo.stars && githubInfo.forks && (
+          {/* {githubInfo.stars && githubInfo.forks && (
             <div className="github-stats">
               <span>
                 <Icon name="Star" />
@@ -113,15 +113,15 @@ const Footer = () => {
                 <span>{githubInfo.forks.toLocaleString()}</span>
               </span>
             </div>
-          )}
+          )} */}
         </a>
       </StyledCredit>
     </StyledFooter>
   );
 };
 
-Footer.propTypes = {
-  githubInfo: PropTypes.object,
-};
+// Footer.propTypes = {
+//   githubInfo: PropTypes.object,
+// };
 
 export default Footer;
